9270da9bb7e4544fdece70a62830d137f7b3f54b
bg5fxp-JF <you@example.com>
2021-07-18 21:14:02 +0100

    entities added, rules and stories edited. More out of scope examples added. Fallback classifier added. *custom action for validating an email needs to be fixed*

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,6 @@
+import re
 from typing import Text, List, Any, Dict
-
-from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk import Tracker, FormValidationAction, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
@@ -11,34 +11,56 @@ class ValidateNameForm(FormValidationAction):
 
     def validate_first_name(
         self,
-        slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         """Validate `first_name` value."""
 
+        first_name = tracker.get_slot("first_name")
         # If the name is super short, it might be wrong.
-        print(f"First name given = {slot_value} length = {len(slot_value)}")
-        if len(slot_value) <= 2:
-            dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
+        print(f"First name given =" + first_name + "length = {len(slot_value)}")
+        if len(first_name) <= 2:
+            dispatcher.utter_message(text="That's a very short name. I'm assuming you mis-spelled.")
             return {"first_name": None}
         else:
-            return {"first_name": slot_value}
+            return {"first_name": first_name}
 
-    # def validate_last_name(
-    #     self,
-    #     slot_value: Any,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: DomainDict,
-    # ) -> Dict[Text, Any]:
-    #     """Validate `last_name` value."""
-    #
-    #     # If the name is super short, it might be wrong.
-    #     print(f"Last name given = {slot_value} length = {len(slot_value)}")
-    #     if len(slot_value) <= 2:
-    #         dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
-    #         return {"last_name": None}
-    #     else:
-    #         return {"last_name": slot_value}
+# class ValidateEmail(Action):
+#     def name(self) -> Text:
+#         return "validate_email_form"
+#
+#     def validate_email(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: DomainDict,
+#     ) -> Dict[Text, Any]:
+#         """Validate `email` value."""
+#
+#         email = tracker.get_slot("email")
+#         # If the name is super short, it might be wrong.
+#         print(f"email given =" + email + "length = {len(slot_value)}")
+#         if re.Match(r"[a-zA-Z0-9_.±]+@[a-zA-Z]+.[^@]+", str(email)):
+#             return {"email": email}
+#         else:
+#             dispatcher.utter_message(text="Invalid email")
+#             return {"email": None}
+
+# class SayName(Action):
+#     def name(self) -> Text:
+#         return "reply_name"
+#
+#     def reply_name(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: DomainDict,
+#     ) -> Dict[Text, Any]:
+#
+#         first_name = tracker.get_slot("first_name")
+#
+#         if len(first_name) <= 0:
+#             dispatcher.utter_message(text=f"You haven't given me your name yet.")
+#         else:
+#             dispatcher.utter_message(text=f"Your name is {first_name}")
